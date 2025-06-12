3da6e27f9fc02a8d611f4c6e94afe1a94b27acb4
francoTNT <francojalilie815@gmail.com>
2025-06-12 16:22:07 -0400

    introduccion de swagger

---
+++ boleta_perfulandia/src/main/java/com/boleta_perfulandia/configuracion/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package com.boleta_perfulandia.configuracion;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(new Info().title("API 2025 Gestion de boletas.").version("0.1")
+                .description("Documentacion de la API para la gestion de boletas."));
+	
+    }
+}
