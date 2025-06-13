a525d963623cb2395f778031c55cd506763633ae
Seba Gonzalez <sebastian.javier.gon@gmail.com>
2025-06-12 23:27:47 -0400

    Update pedidoassembler
    agregue el assembler

--- pedido_perfulandia/src/main/java/com/pedido_perfulandia/config/PedidoAssembler.java
+++ pedido_perfulandia/src/main/java/com/pedido_perfulandia/config/PedidoAssembler.java
@@ -4,14 +4,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 
+import com.pedido_perfulandia.controllador.PedidoControllador;
 import com.pedido_perfulandia.entidad.Pedido;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import java.lang.reflect.Method;
 @Configuration
 public class PedidoAssembler implements RepresentationModelAssembler<Pedido, EntityModel<Pedido>>{
 
 	@Override
 	public EntityModel<Pedido> toModel(Pedido pedido) {
-		return null;
+		return EntityModel.of(pedido,linkTo(methodOn(PedidoControllador.class).obtenerPedido(pedido.getPedidoId())).withSelfRel(),
+				linkTo(methodOn(PedidoControllador.class).editarPedido(pedido.getPedidoId(), pedido)).withRel("Editar pedido"),
+				linkTo(methodOn(PedidoControllador.class).eliminarPedido(pedido.getPedidoId())).withRel("Eliminar pedido"),
+				linkTo(methodOn(PedidoControllador.class).pedidoPorSucursal(pedido.getSucursalId())).withRel("")
+				);
 	}
 
 }
