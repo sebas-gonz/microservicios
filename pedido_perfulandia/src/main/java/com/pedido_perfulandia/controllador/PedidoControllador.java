0cb87505ebbd791e87e606f4fb23968996e7088e
4ndres55-b <martin.hernandez.24.2006@gmail.com>
2025-06-15 19:43:21 -0400

    Documentacion terminada
    Documentacion terminada  al 100 creo

--- pedido_perfulandia/src/main/java/com/pedido_perfulandia/controllador/PedidoControllador.java
+++ pedido_perfulandia/src/main/java/com/pedido_perfulandia/controllador/PedidoControllador.java
@@ -3,6 +3,7 @@ package com.pedido_perfulandia.controllador;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Description;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,7 @@ import com.pedido_perfulandia.entidad.Pedido;
 import com.pedido_perfulandia.servicio.PedidoServicio;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -52,42 +54,125 @@ public class PedidoControllador {
 		@ApiResponses(value = {
 		    @ApiResponse(
 		        responseCode = "200",
-		        description = ""),
+		        description = "Pedido encontrado"),
 		    @ApiResponse(
 		        responseCode = "204",
-		        description = "Datos del pedido erroneos."
+		        description = "No hay pedidos."
 		    )
 		})
+	@Parameter(
+			name="pedidoid",
+			description = "Id del pedido a buscar",
+			required = true,
+			example = "1")
 	public ResponseEntity<Pedido> obtenerPedido(@PathVariable("pedidoid")int pedidoId){
 		Pedido pedido = servicio.pedidoById(pedidoId);
 		return pedido == null ? ResponseEntity.noContent().build() : ResponseEntity.ok(pedido);
 	}
 	
 	@PostMapping("/")
+	@Operation(
+		    summary = "Crear un pedido",
+		    description = "Crear un pedido con sus respectivos datos"
+		)
+		@ApiResponses(value = {
+		    @ApiResponse(
+		        responseCode = "200",
+		        description = "Pedido Creado"),
+		    @ApiResponse(
+		        responseCode = "400",
+		        description = "Datos ingresados erroneos."
+		    )
+		})
 	public ResponseEntity<Pedido> crearPedido(@RequestBody PedidoDTO pedidoDTO){
 		Pedido pedido = servicio.crearPedido(pedidoDTO);
 	    return pedido == null ? ResponseEntity.noContent().build() : ResponseEntity.ok(pedido);
 	}
 	
+	
 	@DeleteMapping("/{pedidoid}")
-	public ResponseEntity<Void> eliminarPedido(@PathVariable("pedidodid")int pedidoId){
+	@Operation(
+		    summary = "Borar un pedido especifico",
+		    description = "Borra un pedido segun el id"
+		)
+		@ApiResponses(value = {
+		    @ApiResponse(
+		        responseCode = "200",
+		        description = "Pedido borrado"),
+		    @ApiResponse(
+		        responseCode = "404",
+		        description = "Pedido no encontrado."
+		    )
+		})
+	@Parameter(
+		    name = "pedidoid",
+		    description = "ID del pedido que se desea eliminar",
+		    required = true,
+		    example = "1001"
+		)
+	public ResponseEntity<Pedido> eliminarPedido(@PathVariable("pedidodid")int pedidoId){
 		servicio.eliminarPedido(pedidoId);
 		return ResponseEntity.noContent().build();
 	}
 	
 	@PostMapping("/{pedidoid}")
+	@Operation(summary ="Editar pedido",
+			   description="Editar los datos de un pedido segun la id")
+		@ApiResponses(value = {
+			@ApiResponse(
+				responseCode = "200",
+				description="Pedido editado correctamente"),
+			@ApiResponse(
+				responseCode = "400",
+				description = "Datos erronoes al editar")		
+		})
+	@Parameter(
+			name="pedidoid",
+			description = "Id del pedido a editar",
+			required = true,
+			example = "1001")
 	public ResponseEntity<Pedido> editarPedido(@PathVariable("pedidoid")int pedidoId,@RequestBody Pedido pedidoAct){
 		Pedido pedido = servicio.editarPedido(pedidoId, pedidoAct);
 		return pedido == null ? ResponseEntity.noContent().build() : ResponseEntity.ok(pedido);
 	}
 	
 	@GetMapping("/sucursal/{sucursalid}")
+	@Operation(summary = "Buscar pedidor por suscursal",
+			   description = "Se buscar todos los pedidos que se realizaron en una sucursal")
+	@ApiResponses(value={
+		@ApiResponse(
+				responseCode = "200",
+				description = "Pedidos encontrados"),
+		@ApiResponse(
+				responseCode = "404",
+				description = "Sucursal no encontrada o sucursal no tiene pedidos")
+	})
+	@Parameter(
+			name="sucursalid",
+			description = "Id de la sucursal a la que queremos ver los pedidos",
+			required = true,
+			example = "1001")
 	public ResponseEntity<List<Pedido>> pedidoPorSucursal(@PathVariable("sucursalid")int sucursalId){
 		List<Pedido> pedidos = servicio.pedidoPorSucursalId(sucursalId);
 		return pedidos != null ? ResponseEntity.ok(pedidos) : ResponseEntity.noContent().build();
 	}
 	
 	@GetMapping("/usuario/{usuarioid}")
+	@Operation(summary = "Buscar pedidor realizados por el usuario",
+			   description = "Traer todos los pedidos realizados, segun el id del usuario")
+	@ApiResponses(value= {
+			@ApiResponse(
+					responseCode = "200",
+					description = "Pedidos encontrados"),
+			@ApiResponse(
+					responseCode = "400",
+					description = "No hay pedidos por el usuario o usuario no existente")
+	})
+	@Parameter(
+			name="usuarioid",
+			description = "Id del usuario al extraer los pedidos",
+			required = true,
+			example="1001")
 	public ResponseEntity<List<Pedido>> pedidoPorUsuario(@PathVariable("usuarioid")int usuarioId){
 		List<Pedido> pedidos = servicio.pedidoPorUsuarioId(usuarioId);
 		return pedidos != null ? ResponseEntity.ok(pedidos) : ResponseEntity.noContent().build();
