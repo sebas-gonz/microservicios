138931cb8f75caebd86fddfad24ca31e2b1c2ec1
4ndres55-b <martin.hernandez.24.2006@gmail.com>
2025-06-30 22:58:15 -0400

    Test de Boleta y trabajador
    Se completa los test de los micro-servicios de: Boleta y Trabajador (empleado)

--- boleta_perfulandia/src/test/java/com/boleta_perfulandia/TestsServicioBoleta.java
+++ boleta_perfulandia/src/test/java/com/boleta_perfulandia/TestsServicioBoleta.java
@@ -1,8 +1,14 @@
 package com.boleta_perfulandia;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
 import java.util.List;
@@ -10,90 +16,90 @@ import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.boleta_perfulandia.entidades.Boleta;
 import com.boleta_perfulandia.repositorio.BoletaRepositorio;
 import com.boleta_perfulandia.servicio.BoletaServicio;
+import com.trabajador_perfulandia.entidad.Empleado;
+@ExtendWith(MockitoExtension.class)
 public class TestsServicioBoleta {
 	@Mock
     private BoletaRepositorio boletaRepository;
 
     @InjectMocks
     private BoletaServicio boletaService;
-
+    private Boleta boleta;
     @BeforeEach
     public void setup(){
-        MockitoAnnotations.openMocks(this);
+       boleta = new Boleta();
+       boleta.setBoletaId(1);
+       boleta.setNombreUsuario("Andrea");
+       boleta.setNombreSucursal("Alameda");
+       boleta.setNombreEmpleado("Maria");
+       boleta.setUsuarioId(1);
+       boleta.setSucursalId(1);
+       boleta.setEmpleadoId(1);
+       boleta.setPedidoId(1);
+       boleta.setTotal(10000);
+      
+        
     }
-
-    @Test
-    public void testGetAllBoletas(){
-        Boleta b1 = new Boleta();
-        b1.setBoletaId(1);
-        b1.setNombreUsuario("Juan");
-
-        Boleta b2 = new Boleta();
-        b2.setBoletaId(2);
-        b2.setNombreUsuario("Ana");
-
-        when(boletaRepository.findAll()).thenReturn(Arrays.asList(b1, b2));
-
-        List<Boleta> resultado = boletaService.obtenerBoletas();
-        assertEquals(2, resultado.size());
+    @Test//Agregar bopleta
+    void agregarBoleta() {
+    	when(boletaRepository.save(any(Boleta.class))).thenReturn(boleta);//Simulamos que el repositorio ingrese una boleta
+    	Boleta crearBoleta = boletaService.crearBoleta(boleta);//Ejecutamos la simulacion
+    	assertNotNull(crearBoleta, "El objeto no puede ser null");//Se verifica que no sea null
+    	assertEquals(1, crearBoleta.getBoletaId());//Se confirma si se ingreso la boleta
     }
-
-    @Test
-    public void testGetBoletaById(){
-        Boleta b = new Boleta();
-        b.setBoletaId(1);
-        b.setNombreUsuario("Pedro");
-
-        when(boletaRepository.findById(1)).thenReturn(Optional.of(b));
-
-        Boleta resultado = boletaService.boletaById(1);
-        assertEquals("Pedro", resultado.getNombreUsuario());
+    @Test //Buscar boleta
+    void buscarBoletaId() {
+    	when(boletaRepository.findById(1)).thenReturn(Optional.of(boleta));//Creamos una simulacion donde el repositorio busca la boleta
+    	Boleta encBoleta = boletaService.boletaById(1);//Se ejecuta la simulacion
+    	assertNotNull(encBoleta,"El objeto no puede ser nulo");//Confirmamos que el objeto no sea nulo
+    	assertEquals(1, encBoleta.getBoletaId());//Confirmamos que la boleta se haya encontrado
     }
-
-    @Test
-    public void testCreateBoleta(){
-        Boleta b = new Boleta();
-        b.setBoletaId(1);
-        b.setNombreUsuario("Maria");
-
-        when(boletaRepository.save(b)).thenReturn(b);
-
-        Boleta resultado = boletaService.crearBoleta(b);
-        assertEquals("Maria", resultado.getNombreUsuario());
+    @Test //Boleta no existente
+    void boletaNoEncontrada() {
+    	when(boletaRepository.findById(3)).thenReturn(Optional.empty());//Simulamos que el repositorio busque una boleta por una ID
+    	Boleta boletaNoEnc = boletaService.boletaById(3);//Se ejecuta el la simulacion
+    	assertNull(boletaNoEnc,"El objeto no puede ser null");//Confirmamos que sea null
+    	
     }
-
-    @Test
-    public void testUpdateBoleta(){
-        Boleta existente = new Boleta();
-        existente.setBoletaId(1);
-        existente.setNombreUsuario("Luis");
-
-        Boleta actualizado = new Boleta();
-        actualizado.setNombreUsuario("Carlos");
-
-        when(boletaRepository.findById(1)).thenReturn(Optional.of(existente));
-        when(boletaRepository.save(existente)).thenReturn(existente);
-
-        Boleta resultado = boletaService.editarBoletaById(1, actualizado);
-        assertEquals("Luis", resultado.getNombreUsuario());
+    @Test //Listar Boletas
+    void listarBoletas() {
+    	List<Boleta> boletas = Arrays.asList(boleta, new Boleta());
+    	when(boletaRepository.findAll()).thenReturn(boletas);
+    	List<Boleta> listBoleta = boletaService.obtenerBoletas();
+    	assertEquals(2, listBoleta.size());
+    	
     }
-
-    @Test
-    public void testDeleteBoleta(){
-        int boletaId = 5;
-
-        boletaService.borrarBoletaById(boletaId);
-
-        verify(boletaRepository).deleteById(boletaId);
+    @Test //Test Editar Boleta
+    void editarBoleta() {
+    	when(boletaRepository.findById(boleta.getBoletaId())).thenReturn(Optional.of(boleta));//Simulamos que el repositorio busque una boleta por el id
+    	Boleta encBoleta = boletaService.boletaById(boleta.getBoletaId());//Ejecutamos la simulacion
+    	encBoleta.setNombreUsuario("Jose");//Actualizamos la boleta
+    	when(boletaRepository.save(encBoleta)).thenReturn(encBoleta);//Simulamos que el repositorio guarde la nueva boleta
+    	Boleta editBoleta = boletaService.editarBoletaById(encBoleta.getBoletaId(), encBoleta);//Ejecutamos la sumulacion
+    	assertNotNull(editBoleta,"El objeto no puede ser nulo");//Confirmamos que el objeto no es nulo
+    	assertEquals(editBoleta.getBoletaId(), boleta.getBoletaId());//Comprobamos que la boleta id sea igual
+    	assertEquals("Jose", boleta.getNombreUsuario()); //Confirmamos que la boleta se haya editato
+    	
+    	
     }
-
-    
-
+    @Test//Test de eliminar una boleta
+    void EliminarEmpleado() {
+    	doNothing().when(boletaRepository).deleteById(1); //Simulamos que en el repositorio se haya borrado una boleta
+    	
+    	boletaService.borrarBoletaById(1); //Ejecutamnos la simulacion
+    	
+    	Boleta elimBoleta = boletaService.boletaById(1);//Buscamos la boleta borrada
+    	assertNull(elimBoleta,"Empleado borrado correctamente");//Aqui verificamos si ña boleta se haya borrado
+    	verify(boletaRepository, times(1)).deleteById(1);;//El metodo test debe de haber sido solo una vez
+    	
+    }
 }
---
+++ empleado_perfulandia/src/test/java/com/trabajador_perfulandia/TestsTrabajadores.java
@@ -0,0 +1,111 @@
+package com.trabajador_perfulandia;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import com.trabajador_perfulandia.entidad.Empleado;
+import com.trabajador_perfulandia.repositorio.EmpleadoRepositorio;
+import com.trabajador_perfulandia.servicio.empleadoServicio;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+@ExtendWith(MockitoExtension.class)
+public class TestsTrabajadores {
+	
+	@Mock
+    private EmpleadoRepositorio repositorio;
+
+    @InjectMocks
+    private empleadoServicio servicio;
+    
+    private Empleado empleado;
+
+    @BeforeEach
+    public void setUp() {
+    	empleado = new Empleado();
+    	empleado.setEmpleadoId(1);
+    	empleado.setNombre("Martin");
+    	empleado.setRut("22147000");
+    	empleado.setCargo("Vendedor");
+    	empleado.setSucursalId(1);
+    	empleado.setCorreo("CorreoAleatorio@Gmail.com");
+    }
+    @Test //Test para ingresar Empleados
+    void agregarEmpleado(){
+    	//Simulamos el registro del repositorio que hacepta cualquier objeto de la entidad Empleado, en este caso retornara el objeto empleado
+    	when(repositorio.save(any(Empleado.class))).thenReturn(empleado);
+    	
+    	Empleado crearEmp = servicio.crearEmpleado(empleado); //Se ejecuta la simulacion
+    	
+    	assertNotNull(crearEmp,"El objeto se deberia haber guradado y tambien se debio retornar");//Se verifica si el Empleado no sea nulo
+    	assertEquals(1,crearEmp.getEmpleadoId());//Se verifica si el usuario se ha ingresado con el id 1
+    }
+    @Test //Buscar empleado por id
+    void buscarEmpPorId() {
+    	when(repositorio.findById(1)).thenReturn(Optional.of(empleado)); //Simulamos para que el repositorio busque por el ID 1
+    	Empleado empleadoBus = servicio.EmpleadoPorId(1);//Se ejecuta la simulacion
+    	assertNotNull(empleadoBus, "El empleado no deberia de ser nulo");//Verificamos que el objeto no sea tipo null
+    	assertEquals(1,empleadoBus.getEmpleadoId());//Se confirma si el empleado se haya encontrado
+    }
+    @Test //Buscar empleado no ingresado
+    void BusEmpleadoNoCreado() {
+    	when(repositorio.findById(3)).thenReturn(Optional.empty()); //Se simula el repositorio para buscar un empleado con el id 3
+    	Empleado noEnc = servicio.EmpleadoPorId(3);//Iniciamos la simulacion
+    	assertNull(noEnc, "El empleado no creado deberia ser nulo");//Se confirma si el empleado  sea nulo
+    	
+    	
+    }
+    @Test //Listar todos los empleados
+    void ListarEmpleados() {
+    	List<Empleado> empleados = Arrays.asList(empleado, new Empleado()); //Creamos una lista con el empleado inicial y los instanciamos
+    	when(repositorio.findAll()).thenReturn(empleados);//Simulamos para que el repositorio traiga una lista
+    	List<Empleado> listaEmpleados = servicio.Empleados();//Se ejecuta la simulacion
+    	assertEquals(2,listaEmpleados.size());//Comprobamos que la lista contenga 2 elementos
+    	
+    }
+    @Test //Editar un empleado
+    void editarEmpledo() {
+    	when(repositorio.findById(empleado.getEmpleadoId())).thenReturn(Optional.of(empleado));//Simulamos que el repositorio encuentre el empleado a editar
+    	Empleado encEmpleado = servicio.EmpleadoPorId(empleado.getEmpleadoId());//Ejecutamos la simulacion
+    	
+    	encEmpleado.setNombre("Andres");//Editamos el empleado
+    	
+    	when(repositorio.save(encEmpleado)).thenReturn(encEmpleado);//Simulamos que el repositorio guarde el empleado editado
+    	
+    	Empleado editEmpl = servicio.editarEmpleado(encEmpleado, encEmpleado.getEmpleadoId()); // Se ejecuta la Actualizacion
+    	
+    	assertNotNull(editEmpl,"Empleado no deberia ser nulo"); //Verificamos que el prodcuto no sea nulo
+    	assertEquals(editEmpl.getEmpleadoId(), empleado.getEmpleadoId());//Verificamos que el empleado sea el mismo que editamos
+    	assertEquals("Andres", empleado.getNombre());//Verificamos si el producto original se  haya editado
+    	
+    	
+    }
+    @Test//Test de eliminar empleado
+    void EliminarEmpleado() {
+    	doNothing().when(repositorio).deleteById(1); //Simulamos que en el repositorio se haya borrado un empleado
+    	
+    	servicio.eliminarEmpleado(1); //Ejecutamnos la simulacion
+    	
+    	Empleado elimEmpleado = servicio.EmpleadoPorId(1);//Buscamos el empleado borrado
+    	assertNull(elimEmpleado,"Empleado borrado correctamente");//Aqui verificamos si el empleado se haya borrado
+    	verify(repositorio, times(1)).deleteById(1);;//El metodo test debe de haber sido solo una vez
+    	
+    }
+
+}
